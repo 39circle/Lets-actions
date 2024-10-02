8749c20590582119976467068a173a7d08fbd1ec
nikki9750 <tamkame123@outlook.com>
2024-10-03 08:53:27 +0900

    なのだをサボらなくなった

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,10 @@ pub fn my_func(range: Range<i32>, func: Box<dyn Fn(i32) -> i32>) -> Vec<i32> {
     res
 }
 
+pub fn nanoda_func(msg: &String) -> String {
+    format!("{}なのだ！", msg)
+}
+
 #[cfg(test)]
 mod simple_func_test{
     use super::simple_func;
@@ -54,5 +58,22 @@ mod my_func_test{
         }));
         let ans = vec![0, 2, 4, 6, 8, 10];
         assert_ne!(data, ans)
+    }
+}
+#[cfg(test)]
+mod nanoda_func_test{
+    use super::nanoda_func;
+    #[test]
+    fn test01() {
+        let msg = String::from("こんにちわ");
+        let ans = String::from("こんにちわなのだ！");
+        let res = nanoda_func(&msg);
+        assert_eq!(res, ans)
+    }
+    #[test]
+    fn test02() {
+        let msg = String::from("こんにちわ");
+        let res = nanoda_func(&msg);
+        assert_ne!(res, msg)
     }
 }
